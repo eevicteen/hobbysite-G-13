81ff5717c8639eacda89472a8875e4c6e0433cd5
Syd <sydpaguio@gmail.com>
2025-03-04 23:45:15 +0800

    Adjusted urls s.t, list view and detail view would be accessible

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import blog_list, blog_details
+
+
+app_name = "blog"
+
+urlpatterns = [
+    path('blog/articles/', blog_list, name='blog_list'),
+    path('blog/articles/<int:id>/', blog_details, name='blog_detail'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def blog_list(request):
+    blog = Blog.objects.all()
+    ctx = {
+        "blogs": blog,
+
+    }
+    return render(request, 'blog_list.html', ctx)
+
+
+def blog_details(request, id):
+    blog = Blog.objects.get(id=id)
+    ctx = {
+        "blog": blog
+    }
+    return render(request, 'blog_details.html', ctx)
