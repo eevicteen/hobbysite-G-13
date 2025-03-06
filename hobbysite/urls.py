a1841861ae64462481616191965f8e36263c051d
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-07 00:15:07 +0800

    replaced merchstore with forum in urls.py

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('merchstore/', include('merchstore.urls', namespace='merchstore'))
+    path('forum/', include('forum.urls', namespace='forum'))
 ]
