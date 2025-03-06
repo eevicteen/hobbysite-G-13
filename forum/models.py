21de911b5736c71aa0c9a0ee98d6ecbe975bd06f
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-07 05:04:16 +0800

    updated PostCategory and Post classes

--- forum/models.py
+++ forum/models.py
@@ -3,15 +3,18 @@ from django.urls import reverse
 
 # Create your models here.
 
-class Ingredient(models.Model):
-    name = models.CharField(max_length=101)
+class PostCategory(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField(_("Description"))
     def __str__(self):
         return self.name
     def get_absolute_url(self):
         return reverse('ledger:ingredient-detail', args=[str(self.pk)])
 
-class Recipe(models.Model):
-    name = models.CharField(max_length=101)
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(PostCategory, on_delete=models.SET_NULL, related_name='recipe')
+
     def __str__(self):
         return self.name
     def get_absolute_url(self):
