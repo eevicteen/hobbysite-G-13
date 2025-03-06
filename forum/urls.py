8effc9321f8948fec3c29d5855e7eae04fccfd46
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-07 00:26:58 +0800

    commented out unimplemented urls and imports

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import threads, thread
+# from .views import threads, thread
 
 urlpatterns = [
-    path('threads', threads, name='list'),
-    path('thread/<int:pk>', thread, name='recipe1'),
+    # path('threads', threads, name='list'),
+    # path('thread/<int:pk>', thread, name='recipe1'),
 ]
 app_name = "forum"
