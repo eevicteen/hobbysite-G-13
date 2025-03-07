81f7eea8ec3067590134a61d063d9493d8c8beaa
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-07 14:39:39 +0800

    registered models and their admin

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
+"""Create needed models and their admin."""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post, PostCategory
+
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    """Create admin for ArticleCategory."""
+
+    model = PostCategory
+
+
+class PostAdmin(admin.ModelAdmin):
+    """Create admin for Article."""
+
+    model = Post
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(PostCategory, PostCategoryAdmin)
