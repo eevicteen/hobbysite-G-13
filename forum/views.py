7b1698251e2e9f015acf3e468c62164b6114583a
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-03-07 14:25:59 +0800

    implemented forum_list and forum_details views

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Post, PostCategory
 
-# Create your views here.
+
+def forum_list(request):
+    """Return blog_list html file with apt context."""
+    categories = PostCategory.objects.all().order_by("name")
+    posts = Post.objects.all().order_by("-creation_date")
+
+    return render(request, "forum_list.html", {
+        "postcategory": categories,
+        "posts": posts
+    })
+
+
+def forum_details(request, id):
+    """Return blog_details html file with apt context."""
+    post = Post.objects.get(id=id)
+    return render(request, "forum_details.html", {"post": post})
