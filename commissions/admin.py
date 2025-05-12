8b47ee71ea13278a6c636af947db71a086d028a6
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-05-12 15:03:33 +0800

    initial implementation of JobAdmin and JobApplicationAdmin

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,9 +1,15 @@
 """Create needed models and their admin."""
 
 from django.contrib import admin
-from .models import Comments, Commission
+from .models import Comments, Commission, Job, JobApplication
 
-
+class CommentsAdmin(admin.ModelAdmin):
+    """Creates admin for comments."""
+    
+    model = Comments
+    search_fields = ('created_on',)
+    list_display = ('entry',)
+    
 class CommentInLine(admin.TabularInline):
     """Creates tabular inline for comments under commission."""
 
@@ -18,14 +24,22 @@ class CommissionAdmin(admin.ModelAdmin):
     list_display = ('title',)
     inlines = [CommentInLine]
 
+class JobAdmin(admin.ModelAdmin):
+    """Creates admin for job."""
+    model=Job
+    search_fields = ('role',)
+    list_display = ('role',)
 
-class CommentsAdmin(admin.ModelAdmin):
-    """Creates admin for comments."""
-    
-    model = Comments
-    search_fields = ('created_on',)
-    list_display = ('entry',)
+class JobApplicationAdmin(admin.ModelAdmin):
+    """Creates admin for job application."""
+    model=JobApplication
+    search_fields = ('applied_on',)
+    list_display = ('applicant',)
 
 
 admin.site.register(Comments, CommentsAdmin)
 admin.site.register(Commission, CommissionAdmin)
+admin.site.register(Job, JobAdmin)
+admin.site.register(JobApplication, JobApplicationAdmin)
+
+
