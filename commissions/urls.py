6529e7eba15aebeb304815aca5ad9e850ec10a29
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-05-15 02:20:38 +0800

    added url for create view

--- commissions/urls.py
+++ commissions/urls.py
@@ -1,12 +1,13 @@
 """Direct users to appropriate urls depending on needs."""
 
 from django.urls import path
-from .views import CommissionListView, commission_detail
+from .views import CommissionListView, commission_detail, create_commission
 
 urlpatterns = [
 
     path('list', CommissionListView.as_view(), name='commissions-list'),
     path('detail/<int:pk>', commission_detail, name='commission-detail'),
+    path('add', create_commission, name='commissions-create'),
 
 ]
 
