e50a97013e91f3f6158d2080c45e325d2fe42d52
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-05-15 02:59:50 +0800

    added filter for jobs

--- commissions/views.py
+++ commissions/views.py
@@ -29,7 +29,7 @@ def commission_detail(request, pk):
     
     commission = get_object_or_404(Commission, pk=pk)
     comments = Comments.objects.filter(commission=commission)
-    jobs = Job.objects.all()
+    jobs = Job.objects.filter(commission=commission)
 
     ctx = {
         "commission": commission,
