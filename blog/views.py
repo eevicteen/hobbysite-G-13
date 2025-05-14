cd25587d161e2d90b592d76068cbd87bf6724f1b
Syd <sydpaguio@gmail.com>
2025-05-14 23:17:05 +0800

    Created universal header in base html with appropriate (albeit wonky) css, adjusted blog html files)

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,6 @@ def blog_list(request):
     })
 
 
-@login_required
 def blog_details(request, id):
     """Return blog_details html file with apt context."""
     article = Article.objects.get(id=id)
@@ -47,7 +46,6 @@ def blog_details(request, id):
         'other_articles': other_articles,
     })
 
-
 @login_required
 def add_article(request):
     """Allows logged-in users to create a new article."""
@@ -62,7 +60,7 @@ def add_article(request):
             return redirect("blog:blog_list")
     return render(request, "add_article.html", {"form": form})
 
-
+@login_required
 def article_update(request, id):
     """Allows logged-in users to update their article."""
     article = get_object_or_404(Article, pk=id)
