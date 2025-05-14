21642925af0ae97ebdf666aca6bbedb034b7cdbd
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-05-15 02:25:59 +0800

    added CommissionCreateForm

---
+++ commissions/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from .models import Commission, Transaction
+
+class CommissionCreateForm(forms.ModelForm):
+    class Meta:
+        model = Commission
+        fields = ['title','author','description','status']
+
+# class RegisterForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ['password','username','email']
+
+# class TransactionForm(forms.ModelForm):
+#     class Meta:
+#         model = Transaction
+#         fields = ['amount']
+
+# class ProductEditForm(forms.ModelForm):
+#     class Meta:
+#         model = Product
+#         fields = ['name','product_type','description','price','stock']
