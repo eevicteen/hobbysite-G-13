93a8ba06900c66416f32969e0d8f69eca17cb7e2
eevicteen <brycerivero@gmail.com>
2025-05-14 19:50:28 +0800

    rearranged forms.py, added password input widget

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 
 class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
-        fields = ['password','username','email']
+        fields = ['username','email','password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
