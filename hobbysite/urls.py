50ec28dae747311df4d7165a8eeee921df320d7a
eevicteen <brycerivero@gmail.com>
2025-05-14 19:43:02 +0800

    added media support for settings urls

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 
@@ -31,3 +33,4 @@ urlpatterns = [
     path('profile/', include('user_management.urls', namespace='profile')),
    
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
