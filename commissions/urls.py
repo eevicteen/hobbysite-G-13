8e92bcbf6f97f8104ee1d80dd5f2763c6023c0fa
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-05-15 12:32:27 +0800

    added path to edit commissions

--- commissions/urls.py
+++ commissions/urls.py
@@ -1,13 +1,14 @@
 """Direct users to appropriate urls depending on needs."""
 
 from django.urls import path
-from .views import CommissionListView, commission_detail, create_commission
+from .views import CommissionListView, commission_detail, create_commission,edit_commission
 
 urlpatterns = [
 
     path('list', CommissionListView.as_view(), name='commissions-list'),
     path('detail/<int:pk>', commission_detail, name='commission-detail'),
     path('add', create_commission, name='commissions-create'),
+    path('<int:pk>/edit', edit_commission, name='commission-edit'),
 
 ]
 
