275401cc9f821b55326bf0a9267b09d53c8ed4a0
Bayani Bernos <rouel.bernos@student.ateneo.edu>
2025-05-15 23:33:51 +0800

    merging wiki-final with main

--- wiki/admin.py
+++ wiki/admin.py
@@ -3,7 +3,19 @@
 
 from django.contrib import admin
 
-from .models import ArticleCategory, Article
+from .models import ArticleCategory, Article, Comment
+
+
+class ArticleInLine(admin.TabularInline):
+    """Tabular inline for articles."""
+
+    model = Article
+
+
+class CommentInLine(admin.TabularInline):
+    """Tabular inline for comments under articles."""
+
+    model = Comment
 
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
@@ -16,6 +28,7 @@ class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
     search_fields = ('name',)
     list_display = ('name', 'description')
+    inlines = [ArticleInLine]
 
 
 class ArticleAdmin(admin.ModelAdmin):
@@ -28,9 +41,29 @@ class ArticleAdmin(admin.ModelAdmin):
 
     model = Article
     search_fields = ('title',)
-    list_display = ('title', 'category')
+    list_display = ('title', 'category', 'created_on', 'updated_on')
     list_filter = ('category',)
+    readonly_fields = ('created_on', 'updated_on')
+    inlines = [CommentInLine]
+
+
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Admin panel config for Comment.
+    
+    Searches using username, title, and entry.
+    Displays author, associated article, and dates.
+    Provides filters for author, associated article,
+    and the creation date.
+    """
+
+    model = Comment
+    search_fields = ('author__display_name', 'article__title', 'entry')
+    list_display = ('author', 'article', 'created_on', 'updated_on')
+    list_filter = ('author', 'article', 'created_on')
+    readonly_fields = ('created_on', 'updated_on')
 
 
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
--- wiki/models.py
+++ wiki/models.py
@@ -1,20 +1,25 @@
 # wiki/models.py
 """
-Wiki app models that define the structures for articles and categories.
+Wiki app models for articles, categories, and comments.
 
 ArticleCategory has a name, description, and has categories ordered by name.
 
 Article has a title, associated category, and entry (the written content).
 Articles have dates for creation and recent updates.
 Articles are sorted in descending order based on creation dates.
+
+Comment has an entry, author, and dates for creation and most recent update.
+Comments are sorted in ascending order based on creation dates.
 """
 from django.db import models
 from django.urls import reverse
 
+from user_management.models import Profile
+
 
 class ArticleCategory(models.Model):
     """
-    Data structure for article categories.
+    Article category model.
 
     Has a name and description.
     """
@@ -34,12 +39,18 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     """
-    Data structure for articles.
+    Article model.
 
-    Has a title, associated category, and entry.
+    Has a title, author, associated category, entry, and header.
     """
 
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name="wiki_articles"
+    )
     category = models.ForeignKey(
         ArticleCategory,
         on_delete=models.SET_NULL,
@@ -48,6 +59,7 @@ class Article(models.Model):
         related_name="articles"
     )
     entry = models.TextField()
+    header_image = models.ImageField(upload_to="article_headers", null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -56,10 +68,36 @@ class Article(models.Model):
         return reverse('wiki:article_details', args=[self.pk])
 
     def __str__(self):
-        """Return title particularly for display purposes."""
+        """Return the article's title."""
         return self.title
 
     class Meta:
         """Sort articles by creation date in descending order."""
 
         ordering = ['-created_on']
+
+class Comment(models.Model):
+    """
+    Comment model.
+
+    Has an entry and associated author & article.
+    """
+
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="wiki_comments"
+    )
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """Sort comments by creation date in ascending order."""
+
+        ordering = ['created_on']
